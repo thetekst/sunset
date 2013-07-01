f7f2a48e14d1e6690b46a986ef01461538102dc7
thetekst <thetekst@gmail.com>
2013-07-01 23:57:06 +0400

    modify

--- index.php
+++ index.php
@@ -10,8 +10,28 @@ function preg_match_sunset($pattern, $str) {
 			$str2 = implode("", $matches);
 			$pattern2 = '/\d{2}:\d{2}/m';
 			if(preg_match($pattern2, $str2, $matches2)) {
-				//file_put_contents('replace.txt',var_export($matches2,true));
-				echo implode('', $matches2);
+				$string_date = implode('', $matches2);
+				$array_hour_and_minute = explode(':', $string_date);
+				if(count($array_hour_and_minute) == 2) {
+					$hour 	= $array_hour_and_minute[0];
+					$minute = $array_hour_and_minute[1];
+					
+					$array_date = explode(',', date("s,m,d,Y"));
+					
+					$second = $array_date[0];
+					$month	= $array_date[1];
+					$day	= $array_date[2];
+					$year	= $array_date[3];
+					
+					$expired = mktime($hour, $minute, $second, $month, $day, $year);
+					$now = time();
+					
+					if($now >= $expired) {
+						echo 'Заход солнца был в '.$string_date.'. Пора бы закрыть окно.';
+					} else {
+						echo 'Заход солнца будут в '.$string_date;
+					}
+				}
 			}
 		}
 	}
